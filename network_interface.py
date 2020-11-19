7176d650a0b3da7bcf62b0c81f839f0340925ed6
Baekjun Kim <baekjun.kim@kaist.ac.kr>
2020-11-19 11:02:45 +0900

    Update network_interface.py
    Now it designed to communicate with Jupiter's media server to ask authentication.

--- network_interface.py
+++ network_interface.py
@@ -5,9 +5,14 @@ It is a server for managing stream IDs.
 import socket
 import stream_id_manager
 import logging
+import time
+from base64 import b64decode
 LOCAL_HOST = '10.0.10.1'
 LOCAL_PORT = 3099
 SIZE = 1024
+ENCRYPTED_MESSAGE_SIZE = 172
+PUBKEY_SIZE = 204
+
 logging.basicConfig(level=logging.DEBUG)
 logging.info('[Server] Import Module')
 
@@ -23,10 +28,20 @@ while True:
 
     msg = client_socket.recv(SIZE)
     logging.debug('[Server] received msg: {}'.format(msg))
-    pubkey = stream_id_manager.issue_streamId(msg)
-    logging.debug('[Server] Issued Stream ID: {}'.format(pubkey))
-    client_socket.sendall(pubkey[8:])
-    logging.info('[Server] Send message to client')
+    #extract
+    public_key = msg[:PUBKEY_SIZE]
+    encrypted_message = msg[PUBKEY_SIZE:PUBKEY_SIZE+ENCRYPTED_MESSAGE_SIZE]
 
+    original_message =stream_id_manager.decrypt_message(\
+            public_key,\
+            b64decode(encrypted_message)).split(':')
+    if (stream_id_manager.check_idpw(original_message[0], original_message[1])):
+        client_socket.sendall('success')
+        logging.info('[Server] Success')
+    else:
+        client_socket.sendall('fail')
+        logging.info('[Server] Failed')
+    logging.info('[Server] Send message to client')
+    time.sleep(1)
     client_socket.close()
     logging.info('[Server] Close client socket')
